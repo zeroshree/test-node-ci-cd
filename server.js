ac0b39d701f7a1a05c0858ebb511e4c44c6580c9
shreeVokal <shree.getvokal@gmail.com>
2018-06-08 17:23:01 +0530

    blue-deployment

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello vokal');
+    res.end('Blue-Deployment');
 }).listen(5001);
